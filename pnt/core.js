6c81773f4a50be031622bd49b92ce11c0cb6ef09
PotatoMaaan <f.r.fabi5x@gmail.com>
2021-12-09 16:04:33 +0100

    first styling and js work

--- pnt/core.js
+++ pnt/core.js
@@ -1,4 +1,18 @@
 // PNT: Extremely light vanilla JS notifications library
 
 // Change settings here
-const config = {};
+const config = {
+	// Specifies if pnt pulls it's default font "Rubik" from google fonts.
+	// If this option is disabled and the user doesn't have the font installed, pnt will use a fallback font
+	pullFromGooglefonts: true,
+};
+
+const linkGoogleFonts = () => {
+	const link = document.createElement("link");
+	link.setAttribute("href", "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+	link.setAttribute("rel", "stylesheet");
+
+	document.head.appendChild(link);
+};
+
+if (config.pullFromGooglefonts) linkGoogleFonts();
