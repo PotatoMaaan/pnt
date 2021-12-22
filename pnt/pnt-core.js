e952444eb2b3b71916c2559bfd953201bd29ee41
PotatoMaaan <f.r.fabi5x@gmail.com>
2021-12-22 23:04:29 +0100

    renamed default message

--- pnt/pnt-core.js
+++ pnt/pnt-core.js
@@ -30,10 +30,7 @@ const pnt = {
 
 	linkGoogleFonts: () => {
 		const link = document.createElement("link");
-		link.setAttribute(
-			"href",
-			"https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-		);
+		link.setAttribute("href", "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,400;1,700&display=swap");
 		link.setAttribute("rel", "stylesheet");
 
 		document.head.appendChild(link);
@@ -42,18 +39,11 @@ const pnt = {
 	ID_PREFIX: "pnt_message:",
 	BUTTON_PREFIX: "pnt_button:",
 	SLIDE_CSS_CLASS_NAME: "pnt_slide_right",
-	CONTAINER_NODE: document.querySelector(".pnt_container")
-		? document.querySelector(".pnt_container")
-		: "",
+	CONTAINER_NODE: document.querySelector(".pnt_container") ? document.querySelector(".pnt_container") : "",
 
 	uuidv4: () => {
 		// Stolen from Stackoverflow, no idea how it works
-		return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-			(
-				c ^
-				(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-			).toString(16)
-		);
+		return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
 	},
 
 	getDomMessageNodes: () => {
@@ -62,13 +52,9 @@ const pnt = {
 
 	buildMessageHtml: (messageID, messageText, messageTitle) => {
 		const html = `
-		<div class="pnt pnt_message_body pnt_slide_right" data-pnt-active="true" id="${
-			pnt.ID_PREFIX + messageID
-		}">
+		<div class="pnt pnt_message_body pnt_slide_right" data-pnt-active="true" id="${pnt.ID_PREFIX + messageID}">
 		<span class="pnt pnt_message_title">${messageTitle}</span>
-		<div class="pnt pnt_message_close_button" title="Close" onClick='pnt.message.removeFromDom("${
-			pnt.ID_PREFIX + messageID
-		}")'>
+		<div class="pnt pnt_message_close_button" title="Close" onClick='pnt.message.removeFromDom("${pnt.ID_PREFIX + messageID}")'>
 			<div class="pnt pnt_message_close_button_element pnt_message_close_button_element_1"></div>
 			<div class="pnt pnt_message_close_button_element pnt_message_close_button_element_2"></div>
 		</div>
@@ -86,13 +72,7 @@ const pnt = {
 		const messageHtml = pnt.buildMessageHtml(currentUUID, text, title);
 
 		if (pnt.config.debug) console.log("(composer) adding to dom: " + domID);
-		pnt.message.addToDom(
-			messageHtml,
-			currentUUID,
-			color,
-			isUserRemoveable,
-			fontColor
-		);
+		pnt.message.addToDom(messageHtml, currentUUID, color, isUserRemoveable, fontColor);
 
 		if (!decayTime == 0) {
 			setTimeout(() => {
@@ -103,37 +83,18 @@ const pnt = {
 	},
 
 	message: {
-		addToDom: (
-			messageHtml,
-			messageID,
-			msgBodyColor,
-			msgisUserRemoveable,
-			msgTextColor
-		) => {
+		addToDom: (messageHtml, messageID, msgBodyColor, msgisUserRemoveable, msgTextColor) => {
 			pnt.message.clearOrphaned();
 			pnt.CONTAINER_NODE.innerHTML += messageHtml;
 			const n_message = document.getElementById(pnt.ID_PREFIX + messageID);
-			n_message.style.setProperty(
-				"--animaton-duration",
-				pnt.config.animationDuration + "ms"
-			);
-			n_message.style.setProperty(
-				"--animation-interpolation",
-				pnt.config.animationInterpolation
-			);
-			if (pnt.config.debug)
-				console.log("adding to dom: " + pnt.ID_PREFIX + messageID);
+			n_message.style.setProperty("--animaton-duration", pnt.config.animationDuration + "ms");
+			n_message.style.setProperty("--animation-interpolation", pnt.config.animationInterpolation);
+			if (pnt.config.debug) console.log("adding to dom: " + pnt.ID_PREFIX + messageID);
 			setTimeout(() => {
 				n_message.classList.remove(pnt.SLIDE_CSS_CLASS_NAME);
-				n_message.style.setProperty(
-					"--message-body-width",
-					pnt.config.containerMaxWidth + "px"
-				);
+				n_message.style.setProperty("--message-body-width", pnt.config.containerMaxWidth + "px");
 				n_message.style.setProperty("--message-color", msgBodyColor);
-				n_message.style.setProperty(
-					"--message-close-button-display-style",
-					msgisUserRemoveable ? "block" : "none"
-				);
+				n_message.style.setProperty("--message-close-button-display-style", msgisUserRemoveable ? "block" : "none");
 				n_message.style.setProperty("--message-text-color", msgTextColor);
 				pnt.message.clearOrphaned();
 			}, 10);
@@ -178,7 +139,7 @@ const pnt = {
 	},
 
 	presets: {
-		message: (title, message, decayTime = 5000) => {
+		success: (title, message, decayTime = 5000) => {
 			pnt.compose(message, title, "green", "white", true, decayTime);
 		},
 		info: (message, title = "Info", decayTime = 5000) => {
